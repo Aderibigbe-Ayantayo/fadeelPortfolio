c44097669e99fa8a8f13b1a9bd3c6ee8f47b0deb
Fadeelson <adex_fadeel@ymail.com>
2024-09-21 07:24:45 -0700

    contact changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-
-// const Contact = () => {
-//     return(
-//         <div className="contact">
-//             <h2>Contact Me</h2>
-//             <p>Email: adex.fadeel1989@gmail.com</p>
-//             <p>Github: <a href="https://github.com/Aderibigbe-Ayantayo">github.com/Aderibigbe-Ayantayo</a></p>
-//             <p>Phone Number: +234-8082698912</p>
-//         </div>
-//     );
-// };
-
-// export default Contact;
-
-
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 import './Contact.css';
